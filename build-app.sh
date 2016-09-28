c11e37e7e22fe0cd2165b32396238cf711ff0e13
beagleknight <david.morcillo@gmail.com>
2016-09-28 10:20:36 +0200

    Use correct rancher binary

--- build-app.sh
+++ build-app.sh
@@ -4,15 +4,19 @@ DECIDIM_GITHUB_URL=$1
 DECIDIM_GITHUB_COMMIT_ID=$2
 
 DECIDIM_APP_NAME="decidim-testapp"
-DECIDIM_PATH="/tmp/decidim"
+TEMP_PATH="/tmp"
+DECIDIM_PATH="$TEMP_PATH/decidim"
+DECIDIM_APP_PATH="$TEMP_PATH/$DECIDIM_APP_NAME"
 
-DOCKER="/tmp/rancher-$RANCHER_VERSION/rancher --host decidim docker"
+RANCHER="/tmp/rancher-$RANCHER_VERSION/rancher"
+DOCKER="$RANCHER --host decidim docker"
 
 RANCHER_STACK="decidim-testapp"
 RANCHER_SERVICE="app"
 
 rm -rf $DECIDIM_PATH
-rm -rf $DECIDIM_PATH/$DECIDIM_APP_NAME
+rm -rf $DECIDIM_APP_PATH
+
 git clone $DECIDIM_GITHUB_URL $DECIDIM_PATH
 cd $DECIDIM_PATH && git checkout $DECIDIM_GITHUB_COMMIT_ID
 
@@ -22,10 +26,10 @@ $DOCKER build -t codegram/decidim $DECIDIM_PATH
 
 $DOCKER push codegram/decidim
  
-$DOCKER run --rm -v $DECIDIM_PATH:/tmp codegram/decidim /tmp/$DECIDIM_APP_NAME
+$DOCKER run --rm -v $TEMP_PATH:/tmp codegram/decidim /tmp/$DECIDIM_APP_NAME
  
 $DOCKER build -t codegram/$DECIDIM_APP_NAME $DECIDIM_PATH/$DECIDIM_APP_NAME
  
 $DOCKER push codegram/$DECIDIM_APP_NAME
 
-rancher up -s $RANCHER_STACK -u -c -d -p $RANCHER_SERVICE
+$RANCHER up -s $RANCHER_STACK -u -c -d -p $RANCHER_SERVICE
