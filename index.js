e3a3a7b19e5cbe496b6bd896f6f6e8d716eadcda
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-09-29 14:39:21 +0200

    Run script using spawn to stream output

--- index.js
+++ index.js
@@ -3,28 +3,14 @@ var GithubWebHook = require('express-github-webhook');
 var webhookHandler = GithubWebHook({ path: '/', secret: process.env.GITHUB_SECRET });
 var bodyParser = require('body-parser');
 
-var sys = require('sys')
-var exec = require('child_process').exec;
-
 var app = express();
+
+var buildApp = require('./lib/build-app.js');
 
 app.use(bodyParser.json());
 app.use(webhookHandler);
 
-webhookHandler.on('push', function (repo, data) {
-  if (repo === "decidim" && data.ref === `refs/heads/${process.env.GITHUB_BRANCH}`) {
-    var githubUrl = data.repository.clone_url;
-    var headCommit = data.head_commit.id;
-
-    exec(`./build-app.sh ${githubUrl} ${headCommit}`, function (err, stdout, stderr) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(stdout);
-      }
-    });
-  }
-});
+webhookHandler.on('push', buildApp);
 
 app.listen(4567, function () {
   console.log('Example app listening on port 4567!');
